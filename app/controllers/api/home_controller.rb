e7a1e27ceb2a45e080e1b8cf7ebcaa5ffbb01902
Ha Thanh Tuan <tuanht@indexmedia.com>
2015-04-16 16:55:51 +0700

    Remove query in Trade's scope. + Move all business logic to model.

--- app/controllers/api/home_controller.rb
+++ app/controllers/api/home_controller.rb
@@ -1,32 +1,5 @@
 class Api::HomeController < Api::ApplicationController
-  include ActionView::Helpers::NumberHelper
-
   def summary
-    import = Trade.summary_import
-    export = Trade.summary_export
-    render :json => {
-               import: import.map do |i|
-                 map_summary i
-               end,
-               export: export.map do |e|
-                 map_summary e
-               end
-           }
-  end
-
-  private
-
-  def map_summary(o)
-    {
-        date: o.date,
-    }.merge get_sum_values(o)
-  end
-
-  def get_sum_values(o)
-    {
-        value: o.sum_value,
-        shipwt: "#{number_to_human o.sum_shipwt} Kilograms",
-        freight_charges: o.sum_freight_charges
-    }
+    render :json => Trade.summary
   end
 end
--- app/models/trade.rb
+++ app/models/trade.rb
@@ -9,15 +9,39 @@ class Trade < ActiveRecord::Base
   enum df: [:undefined, :domestically, :foreign]
   enum table_type: [:with_state_and_port, :with_state_and_commodity, :with_port_and_commodity]
 
-  scope :by_year_and_month, ->(year, month) {where date: Date.new(year, month, 1)}
   scope :with_state_and_port, -> {where table_type: 0}
   scope :with_state_and_commodity, -> {where table_type: 1}
 
-  scope :summary, -> {with_state_and_port.group([:date, :trade_type]).select('date, trade_type, country_id, SUM(value) as sum_value, SUM(shipwt) as sum_shipwt, SUM(freight_charges) as sum_freight_charges')}
-  scope :summary_export, -> {summary.where(trade_type: 1)}
-  scope :summary_import, -> {summary.where(trade_type: 2)}
+  def self.summary
+    sums = with_state_and_port.group([:date, :trade_type]).select('date, trade_type, table_type, country_id, SUM(value) as sum_value, SUM(shipwt) as sum_shipwt, SUM(freight_charges) as sum_freight_charges')
+    sum_export = sums.where trade_type: Trade.trade_types[:export]
+    sum_import = sums.where trade_type: Trade.trade_types[:import]
+    {
+        export: sum_export.map {|s| map_summary s},
+        import: sum_import.map {|s| map_summary s}
+    }
+  end
 
-  scope :by_usa_state, -> {with_state_and_commodity.group([:usa_state_id, :trade_type]).select('trade_type, usa_state_id, SUM(value) as sum_value, SUM(shipwt) as sum_shipwt, SUM(freight_charges) as sum_freight_charges')}
-  scope :by_usa_state_export, -> {by_usa_state.where(trade_type: 1)}
-  scope :by_usa_state_import, -> {by_usa_state.where(trade_type: 2)}
+  def self.summary_sum_value
+    sums = with_state_and_port.group(:trade_type).sum(:value)
+    {
+        export: sums[trade_types[:export]],
+        import: sums[trade_types[:import]],
+    }
+  end
+
+  private
+  def self.map_summary(o)
+    {
+        date: o.date,
+    }.merge get_sum_values(o)
+  end
+
+  def self.get_sum_values(o)
+    {
+        value: o.sum_value,
+        shipwt: o.sum_shipwt,
+        freight_charges: o.sum_freight_charges
+    }
+  end
 end
